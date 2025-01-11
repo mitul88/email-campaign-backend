33153c7ca8b3ad4dcafbef2b9e1c4db026868dce
mitul88 <shahed.rahman88@gmail.com>
2025-01-12 00:34:23 +0600

    index file optimized

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express";
 import routes from "./routes/routes";
 import middleware from "./middleware";
+import { SchedulerService } from "./service/scheduler.service";
+import { emailConsumer } from "./consumer/emailConsumer";
+import { notificationConsumer } from "./consumer/notificationConsumer";
 
 export const app: Application = express();
 
@@ -9,3 +12,12 @@ middleware(app);
 
 // calling all routes
 routes(app);
+
+// initialize cron jobs
+SchedulerService.initializeScheduler();
+
+// start consuming email task
+emailConsumer();
+
+// start consuming notification tasks
+notificationConsumer();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import { ENV_CONFIG } from "./config/env.config";
 import { app } from "./app";
 import { dbConnection } from "./db/connection";
-import { SchedulerService } from "./service/scheduler.service";
-import { emailConsumer } from "./consumer/emailConsumer";
 
 const port = ENV_CONFIG.SERVER.PORT;
-
-// initialize cron jobs
-SchedulerService.initializeScheduler();
-
-// start consuming email task
-emailConsumer();
 
 app.listen(port, () => {
   dbConnection();
