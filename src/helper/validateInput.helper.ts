7c06b49c1f1ac40ba7cfb3034a97e85351b990c5
mitul88 <shahed.rahman88@gmail.com>
2025-01-13 00:31:46 +0600

    user input validate methods added

---
+++ src/helper/validateInput.helper.ts
@@ -0,0 +1,49 @@
+import Joi from "joi";
+interface UserData {
+  name?: string;
+  email: string;
+  password: string;
+  role?: string;
+}
+
+export interface CampaignInputData {
+  name: string;
+  subject: string;
+  message: string;
+  recipients: string[];
+  type: string;
+  scheduleTime: Date;
+}
+
+export const validateUser = (user: UserData) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(255).required(),
+    role: Joi.string().min(5).max(255),
+  });
+
+  return schema.validate(user);
+};
+
+export const validateLogin = (user: UserData) => {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
+
+export const validateCampaignDataInput = (campaign: CampaignInputData) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    subject: Joi.string().min(3).max(200).required(),
+    message: Joi.string().min(5).max(500).required(),
+    recipients: Joi.array().items(Joi.string().required()).max(20),
+    type: Joi.string().min(3).max(255),
+    scheduleTime: Joi.date(),
+  });
+
+  return schema.validate(campaign);
+};
