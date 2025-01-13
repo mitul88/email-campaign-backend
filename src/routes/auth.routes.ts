2f409040909166b16c61e83d03b3fa298c5f50a1
mitul88 <shahed.rahman88@gmail.com>
2025-01-13 15:47:46 +0600

    refresh token routes added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { login, register } from "../controller/auth.controller";
+import {
+  login,
+  refreshTokenHandler,
+  register,
+} from "../controller/auth.controller";
 import { rateLimiterMiddleware } from "../middleware/rateLimiter.middleware";
 
 export const router = Router();
 
 router.route("/login").post([rateLimiterMiddleware], login);
+router.route("/refresh-token").get(refreshTokenHandler);
 router.route("/register").post(register);
